8428ebca8bdde63ceb4fb7a8938a7dc589713105
TranDucLHP-HUST <ductrantrung.hust@gmail.com>
2021-02-02 17:34:00 +0700

    first commit

---
+++ Face_part3.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import sys
+
+recognizer =cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainner/trainner.yml')
+cascadePath = "haarcascade_frontalface_alt.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+studentToID = {}
+# allId = set()
+freq = {}
+
+for i in range(1, len(sys.argv), 2):
+    studentToID[sys.argv[i]] = sys.argv[i + 1]
+cam = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+while True:
+    ret, im =cam.read()
+    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    faces=faceCascade.detectMultiScale(gray, 1.2,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
+        Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+        name = "Unknown"
+        # allId.add(Id)
+        if Id != None and not Id in freq:
+            freq[Id] = 0
+        freq[Id] += 1
+        if(str(Id) in studentToID):
+            name= studentToID[str(Id)]
+        cv2.putText(im,str(name), (x,y+h),font, 0.8, (255,255,255), 1,cv2.LINE_AA)
+    cv2.imshow('im',im)
+    if cv2.waitKey(10) & 0xFF==ord('q'):
+        break
+print(max(freq, key=freq.get))
+
+cam.release()
+cv2.destroyAllWindows()
---
+++ app.js
@@ -0,0 +1,69 @@
+const  express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require("body-parser");
+const {PythonShell} = require('python-shell');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs'); 
+app.use(express.static(path.join(__dirname, 'static')));
+
+let allStudent = [] // { studentID: '123', lastName: 'Kien', firstName: 'Ta', status: true/false}
+let pythonArgs = []
+
+app.get('/', function (req, res) {
+   res.render('trang_chu');
+})
+
+app.get('/them_hs', (req, res) => {
+    res.render('them_hs');
+})
+
+app.post('/them_hs', (req, res) => {
+    req.body.status = false;
+    allStudent.push(req.body);
+    pythonArgs.push(req.body.studentID);
+    pythonArgs.push(req.body.firstName);
+    PythonShell.run('Face_recog.py', {args: [req.body.studentID]}, (err, result) =>{
+        console.log(result)
+        PythonShell.run('Face_part2.py', null, (err, result) =>{
+            console.log(result)
+            res.redirect('/diem_danh');
+        });
+    });
+})
+
+app.get('/diem_danh', (req, res) => {
+    // console.log(pythonArgs)
+    res.render('diem_danh', {allStudent: allStudent});
+})
+
+app.post('/diem_danh', (req, res) => {
+    PythonShell.run('Face_part3.py', {args: pythonArgs}, (err, result) =>{
+        if(result == null || result[0] == null){
+            res.redirect('/diem_danh');    
+        }
+        else{
+            result.forEach(studentID => {
+                let index = allStudent.findIndex(element => element.studentID == studentID)
+		if(index != null && allStudent[index] != null){
+			allStudent[index].status = true
+		}
+            })
+            res.redirect('/diem_danh');
+        }
+
+    });
+})
+
+app.post('/reset', (req, res) => {
+    for(let i = 0; i < allStudent.length; i++){
+        allStudent[i].status = false
+    }
+    res.redirect('/diem_danh');
+})
+
+app.listen(3000, () => {
+    console.log('Application running at port 3000');
+})
